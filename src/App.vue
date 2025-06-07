ef13f174240b63cd216771eab96d59e890cb6c64
kevin <dare.kevin21000@gmail.com>
2025-06-08 00:08:07 +0200

    replace_navbar_flow_bite_by_navbar_by_daisyUI

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,20 @@
 <script setup>
-import {
-  FwbNavbar,
-  FwbNavbarCollapse,
-  FwbNavbarLink,
-  FwbHeading 
-} from 'flowbite-vue'
+
 
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <fwb-navbar class="h-15">
-    <template #logo>
-      <fwb-navbar-link>
-        <RouterLink to="/">
-          <fwb-heading tag="h5">Kevin DA RE</fwb-heading></RouterLink>
-      </fwb-navbar-link>
-    </template>
-    <template #default="{isShowMenu}">
-      <fwb-navbar-collapse :is-show-menu="isShowMenu">
-        <fwb-navbar-link>
-          <RouterLink to="/about">Bio</RouterLink>          
-        </fwb-navbar-link>
-        <fwb-navbar-link >
-          <RouterLink to="/experience">Experience</RouterLink>
-        </fwb-navbar-link>
-        <fwb-navbar-link>
-          <RouterLink to="/skills">Compétences</RouterLink>
-        </fwb-navbar-link>
-        <fwb-navbar-link>
-          <RouterLink to="/contact">Contact</RouterLink>
-        </fwb-navbar-link>
-      </fwb-navbar-collapse>
-    </template>
-  </fwb-navbar>
-  <div class="h-[calc(100vh-4.5rem)] bg-blue-500">
-    <router-view />
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="flex-1">
+    <a >daisyUI</a>
+    <RouterLink class="btn btn-ghost text-xl" to="/">Kevin DA RE</RouterLink>
   </div>
+      <RouterLink class="btn btn-ghost text-xl" to="/experience">Experience</RouterLink>
+      <RouterLink class="btn btn-ghost text-xl" to="/skills">Competences</RouterLink>
+      <RouterLink class="btn btn-ghost text-xl" to="/about">A propos</RouterLink>
+      <RouterLink class="btn btn-ghost text-xl" to="/contact">Contact</RouterLink>
 
+</div>
+   <router-view />
 </template>
