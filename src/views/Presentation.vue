c4766cf0ba9247294563786c1b089244b3d112ad
kevin <dare.kevin21000@gmail.com>
2025-06-18 00:24:36 +0200

    config_div_part_1

--- src/views/Presentation.vue
+++ src/views/Presentation.vue
@@ -1,6 +1,15 @@
 <template>
-<div class="min-h-full bg-amber-600">
-    <h1>Presentation</h1>
+  <div class="flex flex-row justify-around h-full">
+    <div class="p-4 border-2 border-green-700 rounded-lg shadow-md w-max h-50 m-auto">
+      <h1>Bonjour et bienvenue sur mon site !</h1>
+      <h2>Je m'appelle Kevin et je suis développeur Web</h2>  
+    </div>
+    <div class="p-1 border-2 border-green-700 rounded-lg shadow-md w-max h-40 flex flex-col justify-around items-center m-auto">
+
+      <h2 class="mb-2">Télécharger mon CV</h2>
+      <button class="btn btn-outline btn-success">Télécharger mon CV</button>
+
+    </div>
   </div>
 </template>
 
