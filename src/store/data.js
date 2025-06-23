f3d9f6762285d8b0a6c6af621d4424571f2a0b74
DA RE Kévin <k.dare@fimainfo.fr>
2025-06-23 15:44:22 +0200

    experience_in_data.js

---
+++ src/store/data.js
@@ -0,0 +1,24 @@
+export default {
+  experience: [
+    {
+        function: "Operateur sur centre d'usinage",
+        period: '2013-2020',
+        description: 'Fabrication de pièces mécaniques à usage industriel, automobile, ferrovier.',
+        url_img: '../assets/experience/operator_milling_tools.gif'
+    },
+    {
+        function: "Developpeur web",
+        period: '2021-2022',
+        stacks: 'Vuejs, Java',
+        description: "Developpement d'interfae web de gestion de livraison par facturation de colis",
+        url_img: '../assets/experience/web_developper.gif'
+    },
+    {
+        function: "Developpeur integrateur web",
+        period: '2022',
+        stacks: 'JavaScript, SQL',
+        description: "Developpement de solution web pour plateforme de telephonie",
+        url_img: '../assets/experience/web_developper_2.gif'
+    },
+  ]
+};
