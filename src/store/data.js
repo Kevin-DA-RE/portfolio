6b0ddf9eb8a205847c139163ac896a13bd8492cd
kevin <dare.kevin21000@gmail.com>
2025-06-25 23:46:17 +0200

    experience_in_high_screen

--- src/store/data.js
+++ src/store/data.js
@@ -1,24 +1,52 @@
 export default {
   experience: [
     {
-        function: "Operateur sur centre d'usinage",
-        period: '2013-2020',
-        description: 'Fabrication de pièces mécaniques à usage industriel, automobile, ferrovier.',
-        url_img: '../assets/experience/operator_milling_tools.gif'
+      id:1,
+      function: "Operateur sur centre d'usinage",
+      period: '2013-2020',
+      description: 'Fabrication de pièces mécaniques à usage industriel, automobile, ferroviaire.',
+      url_img: 'public/experience/operator_milling_tools.png',
+      tasks: [
+        "Analyse des plans de fabrication",
+        'Programmation de machines à commande numérique',
+        'Contrôle qualité des pièces usinées',
+        'Maintenance préventive et corrective des machines',
+        'Collaboration avec les équipes de production',
+        'Optimisation des processus de fabrication',
+        'Formation des nouveaux opérateurs',
+        'Utilisation de logiciels de CAO/FAO pour la conception et la programmation',
+      ]
     },
     {
-        function: "Developpeur web",
-        period: '2021-2022',
-        stacks: 'Vuejs, Java',
-        description: "Developpement d'interfae web de gestion de livraison par facturation de colis",
-        url_img: '../assets/experience/web_developper.gif'
+      id: 2,
+      function: 'Developpeur web',
+      period: '2021-2022',
+      stacks: 'Vuejs, Java',
+      description: "Developpement d'interface web de gestion de livraison par facturation de colis",
+      url_img: '/public/experience/logistic.png',
+      tasks: [
+        "Analyse des besoins fonctionnels et techniques",
+        'Conception et développement d’interfaces utilisateur',
+        'Intégration de services backend',
+        'Utilisation de frameworks Vuejs',
+      ]
     },
     {
-        function: "Developpeur integrateur web",
-        period: '2022',
-        stacks: 'JavaScript, SQL',
-        description: "Developpement de solution web pour plateforme de telephonie",
-        url_img: '../assets/experience/web_developper_2.gif'
+      id:3,
+      function: 'Developpeur integrateur web',
+      period: '2022',
+      stacks: 'JavaScript, SQL',
+      description: 'Developpement de solution web pour plateforme de telephonie',
+      url_img: '/public/experience/web_developper.png',
+      tasks: [
+        "Analyse des besoins clients",
+        'Conception et développement d’interfaces utilisateur',
+        'Intégration de services backend',
+        'Gestion de bases de données SQL',
+        'Tests et débogage des applications web',
+        'Collaboration avec les équipes de développement',
+        'Mise en production et maintenance des applications',
+      ]
     },
-  ]
-};
+  ],
+}
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,8 +1,29 @@
-<template>
-  <div class="min-h-full bg-amber-600">
-    <h1>Experience</h1>
-  </div>
-</template>
+<script setup>
+import data  from '../store/data'
 
-<style>
-</style>
+
+</script>
+
+<template>
+  <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical overflow-y-auto">
+    <li v-for="(exp, index) in data.experience" :key="exp.id">
+      <div :class="index % 2 === 0 ? 'timeline-end' : 'timeline-start md:mb-10'">
+        <img :src="exp.url_img" :alt="exp.function" class="w-64 h-max rounded-lg object-cover" />
+      </div>
+
+      <div :class="index % 2 === 0 ? 'timeline-start mb-10 md:text-end' : 'timeline-end md:mb-10'">
+        <time class="font-mono italic">{{ exp.period }}</time>
+        <div class="text-lg font-black text-warning">{{ exp.function }}</div>
+        <p class="decoration-sky-500">{{ exp.description }}</p>
+
+        <p v-if="exp.stacks" class="text-sm font-bold mt-1 text-sky-400">Stacks : {{ exp.stacks }}</p>
+
+        <ul v-if="exp.tasks" class="list-disc list-inside text-sm mt-2 space-y-1">
+          <li v-for="(task, idx) in exp.tasks" :key="idx">{{ task }}</li>
+        </ul>
+      </div>
+
+      <hr />
+    </li>
+  </ul>
+</template>
