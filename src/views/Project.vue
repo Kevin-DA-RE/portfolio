32c4e27708b285185074bf1ac574109054ca3183
DA RE Kévin <k.dare@fimainfo.fr>
2025-07-18 14:17:57 +0200

    correctif_slide_for_desktop

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -4,24 +4,30 @@ import data from '../store/data'
 </script>
 
 <template>
+  <!-- Version desktop horizontale -->
   <div class="max-md:hidden carousel w-full h-full animate-fade-in">
     <div
       class="carousel-item relative w-full"
-      v-for="project in data.project"
+      v-for="(project, index) in data.project"
       :key="project.id"
+      :id="`slide${index + 1}`"
     >
-      <CardProject :project="project" />
+      <CardProject :project="project" :index="index" :total="data.project.length" />
     </div>
   </div>
+
+  <!-- Version mobile verticale -->
   <div class="min-md:hidden carousel carousel-vertical rounded-box h-96">
     <div
       class="carousel-item h-full"
-      v-for="project in data.project"
+      v-for="(project, index) in data.project"
       :key="project.id"
+      :id="`slide${index + 1}`"
     >
-      <CardProject :project="project" />
+      <CardProject :project="project" :index="index" :total="data.project.length" />
     </div>
   </div>
 </template>
+
 
 <style></style>
