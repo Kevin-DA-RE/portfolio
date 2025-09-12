301d402994ce00c46246d1f051268972ceb86eb5
DA RE Kévin <k.dare@fimainfo.fr>
2025-09-12 13:38:25 +0200

    delete_Pricing

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ import Home from "./components/Home.vue";
 import Project from "./components/Project.vue";
 import Experience from "./components/Experience.vue";
 import Skill from "./components/Skill.vue";
-import Pricing from "./components/Pricing.vue";
 import Contact from "./components/Contact.vue";
 import Footer from "./components/Footer.vue";
 </script>
@@ -15,7 +14,6 @@ import Footer from "./components/Footer.vue";
   <Skill />
   <Project />
   <Experience />
-  <Pricing />
   <Contact />
   <Footer />
 </template>
