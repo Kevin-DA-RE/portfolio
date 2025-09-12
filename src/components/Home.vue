b5c0abc3a8d223ca45024afd4a8f414ccb9a2ed9
kevin <dare.kevin21000@gmail.com>
2025-09-12 23:04:16 +0200

    template_presentation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,34 +9,50 @@ import { Icon } from "@iconify/vue";
 
 <template>
   <section id="home" class="container">
-    <div class="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
+    <div
+      class="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32"
+    >
       <div class="text-center space-y-8">
-
-        <div class="max-w-screen-md mx-auto text-center text-5xl md:text-6xl font-bold">
+        <div
+          class="max-w-screen-md mx-auto text-center text-5xl md:text-6xl font-bold"
+        >
           <!-- Porttrait -->
-          <img src="../assets/portrait.png" alt="Portrait"
-            class="object-cover rounded-full inline-block mb-3" />
+          <img
+            src="../assets/portrait.png"
+            alt="Portrait"
+            class="object-cover rounded-full inline-block mb-3"
+          />
           <h1>
             Je m'appelle Kevin,<br />
             je suis
-            <span class="text-transparent bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">Développeur Web
-              Fullstack
+            <span
+              class="text-transparent bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text"
+              >Développeur Web Fullstack
             </span>
           </h1>
         </div>
 
         <p class="max-w-screen-sm mx-auto text-xl text-muted-foreground">
-        Passionné par l'univers du développement web,<br />
-        laissez moi vous présenter mes projets et bien plus encore !
+          Passionné par l'univers du développement web,<br />
+          laissez moi vous présenter mes projets et bien plus encore !
         </p>
 
-        <div class="md:space-y-0 md:space-x-4">
+        <div
+          class="flex flex-col h-40 md:flex-row md:h-auto items-center justify-around md:space-x-4"
+        >
           <Button class="w-5/6 md:w-1/4 font-bold group/arrow">
-            Télécharger CV
-            <ArrowRight class="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+            <a
+              href="https://drive.google.com/uc?export=download&id=1AHkEgPogfqium81vowb0_bMrc6zPoVM1"
+              class="font-semibold hover:text-cyan-400 transition-colors"
+            >
+              Télécharger mon CV
+            </a>
           </Button>
 
-          <Button as-child variant="secondary" class="w-5/6 md:w-1/4 font-bold">
+          <Button
+            variant="secondary"
+            class="w-5/6 md:w-1/4 font-bold group/arrow"
+          >
             <a href="https://github.com/Kevin-DA-RE" target="_blank">Github</a>
           </Button>
         </div>
@@ -45,12 +61,12 @@ import { Icon } from "@iconify/vue";
       <div class="relative group mt-14">
         <!-- gradient shadow -->
         <div
-          class="absolute -top-6 right-12 w-[90%] h-12 lg:h-[80%] bg-primary/50 blur-3xl rounded-full img-shadow-animation">
-        </div>
+          class="absolute -top-6 right-12 w-[90%] h-12 lg:h-[80%] bg-primary/50 blur-3xl rounded-full img-shadow-animation"
+        ></div>
         <!-- gradient effect img -->
         <div
-          class="absolute bottom-0 left-0 w-full h-20 md:h-28 bg-gradient-to-b from-background/0 via-background/50 to-background rounded-lg">
-        </div>
+          class="absolute bottom-0 left-0 w-full h-20 md:h-28 bg-gradient-to-b from-background/0 via-background/50 to-background rounded-lg"
+        ></div>
       </div>
     </div>
   </section>
