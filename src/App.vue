d48cf7cedad6c00b4b582d29857e9d199af4450d
DA RE Kévin <k.dare@fimainfo.fr>
2025-09-12 13:35:44 +0200

    delete_FAQ

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@ import Testimonials from "./components/Testimonials.vue";
 import Team from "./components/Team.vue";
 import Pricing from "./components/Pricing.vue";
 import Contact from "./components/Contact.vue";
-import FAQ from "./components/FAQ.vue";
 import Footer from "./components/Footer.vue";
 </script>
 
@@ -24,6 +23,5 @@ import Footer from "./components/Footer.vue";
   <Team />
   <Pricing />
   <Contact />
-  <FAQ />
   <Footer />
 </template>
