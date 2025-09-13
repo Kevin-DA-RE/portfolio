69841c29405aab16e2fc13259c35099291f09171
kevin <dare.kevin21000@gmail.com>
2025-09-13 21:52:01 +0200

    id_number

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 
 interface HowItWorksProps {
+  id: number;
   badgeTitle: string;
   title: string;
   period: string;
